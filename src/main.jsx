5fce78a1c70bf24ffd8f1528ee27a4c6d85968e2
khan Alfiya khatoon <102402466+kasa677@users.noreply.github.com>
2026-03-13 17:52:13 +0530

    main update

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
-
 import { AuthProvider } from "./context/AuthContext";
-import { CartProvider } from "./context/CartContext"; 
-
+import { CartProvider } from "./context/CartContext";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <CartProvider> 
-       
- <App />
-           
-         
-      
-        <Toaster position="top-center" />
-      </CartProvider>
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <CartProvider>
+          <App />
+          <Toaster position="top-center" />
+        </CartProvider>
+      </AuthProvider>
+    </HelmetProvider>
   </StrictMode>
 );
