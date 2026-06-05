ae1b9156fbe54c484b95851a22033b45e5693fc2
khan Alfiya khatoon <102402466+kasa677@users.noreply.github.com>
2026-06-06 01:10:21 +0530

    checkoutfix

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
 import { useCart } from "../context/CartContext";
 import { 
-  FaArrowLeft,FaUtensils ,FaLock ,FaSpinner,FaMotorcycle,FaMapMarkedAlt,FaTimes, FaCrosshairs ,FaMapMarkerAlt
+  FaCrown, FaArrowLeft,FaUtensils ,FaLock ,FaSpinner,FaMotorcycle,FaMapMarkedAlt,FaTimes, FaCrosshairs ,FaMapMarkerAlt
 
 } from "react-icons/fa";
 import {
